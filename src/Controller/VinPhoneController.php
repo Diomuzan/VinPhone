87a0d162d58491e4fed80ac1194a43fef83324c2
rincedal <94070451+Diomuzan247@users.noreply.github.com>
2023-05-05 22:30:02 +0200

    Removed phones from database

--- src/Controller/VinPhoneController.php
+++ src/Controller/VinPhoneController.php
@@ -8,8 +8,8 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class VinPhoneController extends AbstractController {
-    #[Route('/VinPhone', name: 'VinPhone')]
-    public function vinphone(EntityManagerInterface $entityManager, int $id = null): Response {
+    #[Route('/VinPhone/Home', name: 'Home')]
+    public function home(EntityManagerInterface $entityManager, int $id = null): Response {
 
         if ($id !== null) {
             $phones = $entityManager->getRepository(Vinphone::class)->find($id);
@@ -17,14 +17,34 @@ class VinPhoneController extends AbstractController {
             $phones = $entityManager->getRepository(Vinphone::class)->findAll();
         }
 
-        return $this->render('VinPhone.html.twig', ['phones' => $phones]);
+        return $this->render('VinPhone_Home.html.twig', ['phones' => $phones]);
     }
 
-    #[Route('/VinPhone/AddPhone', name: 'AddPhone')]
+    #[Route('/VinPhone/Add', name: 'Add')]
     public function new(EntityManagerInterface $entityManager, int $id = null): Response {
 
-        return $this->render('VinPhone_AddPhone.html.twig');
+
+        return $this->render('VinPhone_Add.html.twig');
     }
 
+    #[Route('/VinPhone/Delete', name: 'Delete')]
+    public function delete(EntityManagerInterface $entityManager, int $id = null): Response {
 
+
+        return $this->render('VinPhone_Delete.html.twig');
+    }
+
+    #[Route('/VinPhone/Detail', name: 'Detail')]
+    public function detail(EntityManagerInterface $entityManager, int $id = null): Response {
+
+
+        return $this->render('VinPhone_Detail.html.twig');
+    }
+
+    #[Route('/VinPhone/Edit', name: 'Edit')]
+    public function edit(EntityManagerInterface $entityManager, int $id = null): Response {
+
+
+        return $this->render('VinPhone_Edit.html.twig');
+    }
 }
